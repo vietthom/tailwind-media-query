05ef4610820e086223372d2620a6627c1e04badf
vietthom <thomle0418@gmail.com>
2022-09-18 14:46:55 -0600

    simplified mana-team UI layout using flex box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex gap-4 items-center">
+      <div className="h-12 w-12 rounded-full object-cover px-2 bg-red-500">img</div>
+      <div className="px-2 bg-yellow-500">Name</div>
+      <div className="flex grow px-2 bg-blue-500">
+        <div className="px-2 bg-green-500">Interests</div>
+        <div className="px-2 bg-orange-500">Social Media links</div>
+      </div>
+      <div className="px-2 bg-purple-500">date</div>
     </div>
   );
 }
